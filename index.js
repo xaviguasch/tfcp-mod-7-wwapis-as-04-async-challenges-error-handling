430b57a940eb8037e91ffcdb55d7b0a6d2c6c69e
xaviguasch <xaviguasch@gmail.com>
2020-08-12 12:13:48 +0200

    added initial files

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById('x').addEventListener('click', () => {
+  const userApi = 'https://randomuser.me/apii'
+
+  fetch(userApi)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log('promise chaining:', json.results[0].name.first)
+    })
+})
+
+document.getElementById('y').addEventListener('click', async () => {
+  const userApi = 'htttps://randomuser.me/api/'
+
+  const response = await fetch(userApi)
+  const json = await response.json()
+  console.log('async/await:', json.results[0].name.first)
+})
